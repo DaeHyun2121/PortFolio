319ef315b865942ad282741c90069e7e1b4be52e
wheogus9611 <wheogus9611@naver.com>
2020-11-10 15:48:39 +0900

    section1

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Side Navi
+function openNav() {
+  document.getElementById("Sidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("Sidenav").style.width = "0%";
+}
